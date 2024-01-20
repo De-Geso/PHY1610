e67c5cdd03357e6173f8006e0f7391bd4ac4e99a
Brad Friesen <bradleyfriesen17@gmail.com>
2024-01-20 14:35:13 -0500

    First commit for PHY1610. Assignment 1 code is working, but incomplete. RK4 has been successfully implemented, the next step is to add a bracket search to find the optimal angle. I want to take the RK4 driver out of main and put it into its own function.

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+// @file helloworld.cpp
+// Hello world rogram in C++
+// on cluster use:
+// ssh USERNAME@teach.scinet.utoronto.ca
+// module load gcc/12
+// Compile with g++ -std=c++17
+#include <iostream>
+using std::cout;
+
+int main()
+{
+	cout << "Hello, world!\n";
+}
