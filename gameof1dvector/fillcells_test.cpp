d860d93b052a2c55be73df37bf318f33485e9aba
SciNet phy1610 course account <lcl_uotphy1610s2467@teach02.scinet.local>
2024-02-07 22:17:14 -0500

    I think I figured out how to write out the correct format. All alive and all dead tests work.

--- gameof1dvector/fillcells_test.cpp
+++ gameof1dvector/fillcells_test.cpp
@@ -7,9 +7,14 @@
 TEST_CASE("fillcells test")
 {
     int n = 5;
-    
+
+    // Test case where all cells are alive.    
     Cells all_alive(n);
     fill(all_alive.begin(), all_alive.end(), "alive");
-    
     REQUIRE( initial_cells(n, 1.0) == all_alive );
+
+    // Test case where all cells are dead.
+    Cells all_dead(n);
+    fill(all_dead.begin(), all_dead.end(), "dead");
+    REQUIRE( initial_cells(n, 1.0) == all_dead );    
 }
